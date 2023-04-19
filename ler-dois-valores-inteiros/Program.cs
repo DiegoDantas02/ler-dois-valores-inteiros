b6de3b3ddc329b69ca9fbe4327414a779d97dde2
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-04-19 20:44:32 -0300

    Add project files.

---
+++ ler-dois-valores-inteiros/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a, b, temp;
+
+        Console.Write("Digite o valor de A: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Digite o valor de B: ");
+        b = int.Parse(Console.ReadLine());
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        Console.WriteLine("Os valores trocados são: A = " + a + " e B = " + b);
+    }
+}
